380a679634e392dc8923a3035d892c91cc3db17a
Daniel Sayer <daniel.sayer01@gmail.com>
2026-06-26 14:51:27 +1000

    chore: remove pwa devOptions

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -46,7 +46,6 @@ export default defineConfig({
         categories: ["health", "fitness", "sports"],
       },
       pwaAssets: { disabled: false, config: true },
-      devOptions: { enabled: true },
     }),
   ],
 });
